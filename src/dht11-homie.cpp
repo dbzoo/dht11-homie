77acf2c821f4d0b856c46d679ba67a6c9df5ba6e
Brett England <brett@dbzoo.com>
2019-01-07 13:26:40 +0000

    Switched to DHTNew library for stability

--- src/dht11-homie.cpp
+++ src/dht11-homie.cpp
@@ -1,34 +1,48 @@
-#include <DHT.h>
+#include <DHTNew.h>
 #include <Homie.h>  // homie-esp8266-3.0.1
 
 HomieSetting<long> intervalSetting("interval", "The interval in seconds");
-const int DEFAULT_INTERVAL = 300;
+const int DEFAULT_INTERVAL = 60;
 unsigned long lastSent = 0;
-
+unsigned long dhtErrors = 0;
+unsigned long dhtReadings = 0;
 #define DHTPIN D7 // NodeMCU pin
 
-#define DHTTYPE DHT11 // Adjust range if you change this
+#define DHTTYPE DHT_MODEL_DHT11 // Adjust range if you change this
 DHT dht(DHTPIN, DHTTYPE);
 
 HomieNode temperatureNode("temperature", "Temperature", "temperature");
 HomieNode humidityNode("humidity", "Humidity", "humidity");
+HomieNode errorNode("errors", "Errors", "errors");
+HomieNode readingsNode("readings", "Readings", "readings");
 
 void loopHandler() {
-  if (millis() - lastSent >= intervalSetting.get() * 1000UL || lastSent == 0) {
-    // Read temperature as Celsius (the default)
-    float temperature = dht.readTemperature();    
-    float humidity = dht.readHumidity();
-	
-    // Check if any reads failed and exit early.
-    if (isnan(humidity) || isnan(temperature)) {
-      Homie.getLogger().println("Failed to read from DHT sensor!");
+  if (! (millis() - lastSent >= intervalSetting.get() * 1000UL || lastSent == 0)) {
+    return;
+  }
+
+  if(dht.read()) {
+    dhtReadings++;
+    readingsNode.setProperty("count").send(String(dhtReadings));
+
+    if(dht.getError() != DHT_ERROR_NONE) {
+      dhtErrors++;
+      errorNode.setProperty("count").send(String(dhtErrors));
+      Homie.getLogger().println( dht.getErrorString());
+      // wait for the minimum sampling period
+      delay(dht.getMinimumSamplingPeriod());
       return;
     }
+
+    // Read temperature as Celsius (the default)
+    float temperature = dht.getTemperature();    
+    float humidity = dht.getHumidity();
+	
     Homie.getLogger() << "Temperature: " << temperature << " °C" << endl;
     temperatureNode.setProperty("degrees").send(String(temperature));
     Homie.getLogger() << "Humidity: " << humidity << " %" << endl;
     humidityNode.setProperty("percent").send(String(humidity));
-
+    
     lastSent = millis();
   }
 }
@@ -40,7 +54,9 @@ void setup() {
   Homie.setLoopFunction(loopHandler);
   temperatureNode.advertise("degrees").setName("Degrees").setDatatype("float").setUnit("°C");
   humidityNode.advertise("percent").setName("Percent").setDatatype("float").setUnit("%");
-    
+  errorNode.advertise("count").setName("Errors").setDatatype("float");
+  readingsNode.advertise("count").setName("Readings").setDatatype("float");
+
   intervalSetting.setDefaultValue(DEFAULT_INTERVAL).setValidator([] (long candidate) {
     return candidate > 0;
   });
